66447702394a67e7d9372893c2d298dbac9cb640
nasiremon011 <nasiremon011@gmail.com>
2022-04-13 11:36:15 +0600

    init

---
+++ Module/simpleMath.py
@@ -0,0 +1,11 @@
+def add(num1:int, num2:int)->int:
+    return num1 + num2
+
+
+def sub(num1:int, num2:int)->int:
+    return num1 - num2
+
+var = 9999 
+
+if __name__ == "__main__":
+    print("Something")
